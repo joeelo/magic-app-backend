b277e92e7c73490762dbacd86cabfaba8f8142bd
Joseph Lorenzo <josephclorenzo@gmail.com>
2019-05-12 09:28:44 -0400

    adding routes and will to whill scale

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -1,15 +1,26 @@
 const express = require(`express`);
+const app = express();
 const router = new express.Router();
 const mtg = require('mtgsdk');
+const cors = require("cors");
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 router.get(`/collection/:name?`, async (req, res) => {
   const cards = await mtg.card.where({name: req.query.name})
   try {
     res.send(cards);
-
   } catch (error) {
     res.status(400).send(error);
   }
 })
+
+// router.post(`/library`, async (req, res) => {
+
+// })
 
 module.exports = router;
