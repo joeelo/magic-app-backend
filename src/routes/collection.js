8ed208c864ae2057902c708adab73e8d46f28b37
Joseph <josephclorenzo@gmail.com>
2019-05-14 09:38:34 -0400

    commit before merge

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const router = new express.Router();
 const mtg = require('mtgsdk');
+const cors = require("cors");
 
 router.get(`/collection/:name?`, async (req, res) => {
   const cards = await mtg.card.where({name: req.query.name})
