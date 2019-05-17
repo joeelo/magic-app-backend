bc123afe8657da649ff9ee916bb8caf7a0989d0f
Joseph <josephclorenzo@gmail.com>
2019-05-17 13:05:19 -0400

    working on getting user to DB

--- src/routes/collection.js
+++ src/routes/collection.js
@@ -4,6 +4,7 @@ const router = new express.Router();
 const mtg = require('mtgsdk');
 const cors = require("cors");
 
+app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
